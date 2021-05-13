02b4fc874e4573a9291690c25267bdd5e78497c2
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-05-13 21:21:41 +0200

    backlog

--- wget.sh
+++ wget.sh
@@ -3,17 +3,16 @@
 # usage: wget.sh url
 # 20210118 erikt(at)xs4all.nl
 
-URL=richtlijnendatabase.nl
+URL=https://richtlijnendatabase.nl
 LOGFILE=logfile.`date '+%Y%m%d'`
 BASEDIR="/home/erikt/projects/przona/data/"
 
-if [ -z "$URL" ]
+if [ -n "$1" ]
 then
-   echo "usage: wget.sh url" >&2
-   exit 1
+   URL=$1
 fi
 
 cd $BASEDIR
-# wget -nc -r -e robots=off -o - "$URL" 2>&1 >> $LOGFILE
 wget -nc -r -o - -R "*.pdf" -X /contact,/info,/pdf,/assets,/gerelateerde_documenten,/en/contact,/en/info,/en/pdf,/en/assets,/en/gerelateerde_documenten "$URL" 2>&1 | tee $LOGFILE
+
 exit 0
